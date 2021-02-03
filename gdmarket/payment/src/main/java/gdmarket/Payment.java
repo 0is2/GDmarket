a1c5829b6a0087ae3b7d62a62cd261272eb50b2d
0is2 <youngneee@gmail.com>
2021-02-03 13:28:23 +0900

    Update Payment.java

--- gdmarket/payment/src/main/java/gdmarket/Payment.java
+++ gdmarket/payment/src/main/java/gdmarket/Payment.java
@@ -41,7 +41,7 @@ public class Payment {
             paymentCompleted.publishAfterCommit();
 
             try {
-                Thread.currentThread().sleep((long) (1000 + Math.random() * 220));
+                Thread.currentThread().sleep((long) (400 + Math.random() * 220));
                 System.out.println("=============결제 승인 완료=============");
             } catch (InterruptedException e) {
                 e.printStackTrace();
